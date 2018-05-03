c7a2c6bfa247d5522b1cd672347773eb9fd38d5f
sangwondee <sangwondee@gmail.com>
2018-05-04 01:26:36 +0700

    Format data in Resource->Article

--- app/Http/Resources/Article.php
+++ app/Http/Resources/Article.php
@@ -14,6 +14,11 @@ class Article extends Resource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'lname'=> $this->lname
+        ];
     }
 }
--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Article::class, function (Faker $faker) {
     return [
         //
-        'name' => $faker->text(20),
-        'lname' => $faker->text(20)
+        'name' => $faker->name,
+        'lname' => $faker->lastname
     ];
 });
